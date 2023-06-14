4cb43856cde8e1ae51d9dc4e72a00a3952125a3a
YaroslavKSE <ikhomych@kse.org.ua>
2023-06-14 17:24:09 +0300

    Add project files.

---
+++ Models/Contact.cs
@@ -0,0 +1,20 @@
+﻿namespace PYMB_Web.Models
+{
+    public class Contact
+    {
+        public Contact(int id, string name, string surname, string address, string email)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Address = address;
+            Email = email;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+    }
+}
